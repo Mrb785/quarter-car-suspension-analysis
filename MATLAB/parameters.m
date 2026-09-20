8730df8236279fe8917c31ed4c1e28e01940c3ef
Maitreya <maitreyabirhade@gmail.com>
2026-09-20 22:55:31 +0530

    Add parameters.m

---
+++ MATLAB/parameters.m
@@ -0,0 +1,12 @@
+%% Quarter-Car Model Parameters
+
+% Vehicle masses
+ms = 290;          % Sprung mass (kg)
+mu = 59;           % Unsprung mass (kg)
+
+% Suspension parameters
+ks = 16182;        % Suspension stiffness (N/m)
+cs = 1000;         % Suspension damping (Ns/m)
+
+% Tire stiffness
+kt = 190000;       % Tire stiffness (N/m)
